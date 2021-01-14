92bfaba25e50203aa356574ae3fe90a247424253
Laura <laurakriley01@gmail.com>
2021-01-14 08:15:45 -0700

    Add project files.

---
+++ DayTwoExample.vb
@@ -0,0 +1,20 @@
+﻿Module DayTwoExample
+
+    Sub Main()
+        'If you hover over a main word and hit F1 it will open up a help thing in the browser
+
+        'Makes it Beep(Frequency, Duration in Milliseconds)
+        Console.Beep(400, 2000)
+
+        'Writes "Hello World onto a screen
+        Console.WriteLine("Initiate")
+
+        'You can click on the grey part of the screen to put a break in the code
+        'In oder to step through you have to hit F11
+        Console.WriteLine("Hello, World")
+
+        'Makes the program wait until the user does something
+        Console.Read()
+    End Sub
+
+End Module
